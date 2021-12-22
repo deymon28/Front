3a30d2464291c2e6e3e558be1425730724732d41
deymon <dimag2811@gmail.com>
2021-12-22 14:45:57 +0200

    s

---
+++ project/build/precache-manifest.a0fb3033df65c3721bdf13b49cccefcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74e707638262225c4d21",
+    "url": "/static/css/main.240487af.chunk.css"
+  },
+  {
+    "revision": "74e707638262225c4d21",
+    "url": "/static/js/main.74e70763.chunk.js"
+  },
+  {
+    "revision": "b4767ccc23c6d868a37b",
+    "url": "/static/js/1.b4767ccc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c158ca149051dd552b44ddfee71f46b",
+    "url": "/index.html"
+  }
+];
